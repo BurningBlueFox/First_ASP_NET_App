32bc456ac4c901fdb0da25127627aabdd1310084
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2020-05-05 11:35:13 -0300

    List items on main page

--- CotosoCrafts.Website/Pages/Index.cshtml.cs
+++ CotosoCrafts.Website/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CotosoCrafts.Website.Models;
+using CotosoCrafts.Website.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,15 +13,18 @@ namespace CotosoCrafts.Website.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductsService _productService;
+        public IEnumerable<Product> _products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         public void OnGet()
         {
-
+            _products = _productService.GetProducts();
         }
     }
 }
